2bc1359c17da14e80d82b2018d96a9537cf9fd6b
Istheaksayem <istheakahmed9345@gmail.com>
2026-02-03 08:20:13 +0600

    added a user collection

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require('express')
-const cors =require("cors")
+const cors = require("cors")
 const app = express()
 require('dotenv').config();
-const port =process.env.PORT || 5000
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ba90y0b.mongodb.net/?appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db('blood_donation')
+    const usersCollection = db.collection('users')
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      // prevent duplicate user
+      const existingUser = await usersCollection.findOne({ email: user.email });
+      if (existingUser) {
+        return res.send({ message: 'user already exists' });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Blood donation!')
